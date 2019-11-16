ea09e03bbf38399d127f67ae721f69624615159a
Allan Cheng <allanhfcheng@gmail.com>
2019-11-15 19:55:06 -0800

    session2

---
+++ src/app.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+const { NODE_ENV } = require('./config')
+const showroute = require('./show/show-router')
+//const noteroute = require('./notes/notes-router')
+
+
+const app = express()
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+app.use('/shows',showroute)
+//app.use('/notes',noteroute)
+app.get('/', (req, res) => {
+       res.send('Hello, world!')
+     })
+    
+
+     app.use(function errorHandler(error, req, res, next) {
+           let response
+           if (NODE_ENV === 'production') {
+             response = { error: { message: 'server error' } }
+           } else {
+             console.error(error)
+             response = { message: error.message, error }
+           }
+           res.status(500).json(response)
+         })
+
+
+
+module.exports = app
---
+++ src/show/show-router.js
@@ -0,0 +1,105 @@
+const express = require('express')
+//const xss = require('xss')
+const ShowsService = require('./show-service')
+
+const showsRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeShow = show => ({
+  id: show.id,
+  showname: show.name,
+  startdate: show.startdate,
+  finishdate: show.finishdate,
+  genre: show.genre,
+  seasons: show.seasons,
+  showdescription: show.showdescription,
+  showlanguage: show.language,
+  currentseason: show.currentseason
+})
+
+showsRouter
+  .route('/')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ShowsService.getAllShows(knexInstance)
+      .then(shows => {
+        res.json(shows.map(serializeFolder))
+      })
+      .catch(next)
+  })
+  .post(jsonParser, (req, res, next) => {
+   // const {  showname,startdate,finishdate, genre,seasons,showdescription,showlanguage,currentseason } = req.body
+    
+    const newShow = req.body
+    ShowsService.insertShow(
+      req.app.get('db'),
+      newShow
+    )
+      .then(show => {
+        console.log(show)
+        res
+          .status(201)
+         // .location(`/folders/${folder.id}`)
+          .json(serializeShow(show))
+      })
+      .catch(next)
+  })
+
+showsRouter
+  .route('/:show_id')
+  .all((req, res, next) => {
+    ShowsService.getById(
+      req.app.get('db'),
+      req.params.show_id
+    )
+      .then(show => {
+        if (!show) {
+          return res.status(404).json({
+            error: { message: `Folder doesn't exist` }
+          })
+        }
+        res.show = show
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeShow(res.folder))
+  })
+  .delete((req, res, next) => {
+    ShowsService.deleteShow(
+      req.app.get('db'),
+      req.params.show_id
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+
+   .patch(jsonParser, (req, res, next) => {
+       const { showname } = req.body
+       const showToUpdate = { showname }
+    
+
+       const numberOfValues = Object.values(showToUpdate).filter(Boolean).length
+          if (numberOfValues === 0) {
+            return res.status(400).json({
+              error: {
+                message: `Request body must contain either 'title', 'style' or 'content'`
+              }
+            })
+          }
+    
+       ShowsService.updateShow(
+         req.app.get('db'),
+         req.params.show_id,
+         showToUpdate
+       )
+         .then(numRowsAffected => {
+           res.status(204).end()
+         })
+         .catch(next)
+      })
+
+module.exports = showsRouter
