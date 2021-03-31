04ad4ddea6335cb196762c2fd4c70eb56150fe3d
Allan Cheng <allanhfcheng@gmail.com>
2021-03-31 01:23:41 -0700

    test

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use('/shows', showroute);
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello, world! Hi!');
 })
 
 app.use(function errorHandler(error, req, res, next) {
